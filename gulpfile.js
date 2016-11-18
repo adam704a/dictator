6535efa1dedb2b5b77cdc797bef1723006c6bc24
Adam Preston <adam.m.preston@gmail.com>
2016-11-17 21:25:36 -0700

    autoinstall semantic

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var
+  gulp  = require('gulp'),
+  watch = require('./semantic/tasks/watch'),
+  build = require('./semantic/tasks/build')
+;
+// import task with a custom task name
+gulp.task('watch ui', watch);
+gulp.task('build ui', build);
