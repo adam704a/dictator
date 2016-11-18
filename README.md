a752feef0ab8a2489d1e39d9b8b4c50484ee6758
Adam Preston <adam.m.preston@gmail.com>
2016-11-17 22:17:50 -0700

    reference semantic from public

---
+++ README.md
@@ -0,0 +1,3 @@
+# Running Dictator
+
+`gulp build`
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,19 @@ var
   watch = require('./semantic/tasks/watch'),
   build = require('./semantic/tasks/build')
 ;
+
 // import task with a custom task name
 gulp.task('watch ui', watch);
 gulp.task('build ui', build);
+
+gulp.task('build', ['build ui'], function () {
+ 
+  var stream = gulp.src(['semantic/dist/**/*'])
+        // do some concatenation, minification, etc.
+        .pipe(gulp.dest('public/semantic/'));
+    return stream; // return the stream as the completion hint
+
+
+});
+
+
